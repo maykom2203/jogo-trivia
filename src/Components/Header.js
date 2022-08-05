0aa4fecfd541295627d441c326d1dfb2edfae5f7
Eduardo Simões <eduardorsimoes97@gmail.com>
2022-08-05 19:12:23 -0300

    Importado PropTypes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import md5 from 'crypto-js/md5';
 
 class Header extends Component {
