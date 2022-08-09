86817d401a7c320114d407c87025c512fbde2180
Taisa Mattiazzi Ferreira <ferreirataisa@yahoo.com.br>
2022-08-09 20:33:18 -0300

    Requisito 18 inicializado - encontrar pq do erro

--- src/pages/Ranking.js
+++ src/pages/Ranking.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 
-class Ranking extends React.Component {
+class Ranking extends Component {
+  constructor() {
+    super();
+    this.state = ({
+      goLogin: false,
+    });
+    this.goToLogin = this.goToLogin.bind(this);
+  }
+
+  goToLogin() {
+    this.setState({
+      goLogin: true,
+    });
+  }
+
   render() {
+    const { goLogin } = this.state;
     return (
-      <h1 data-testid="ranking-title">Ranking</h1>
+      <div>
+        <h1 data-testid="ranking-title">Ranking</h1>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.goToLogin }
+        >
+          Login
+        </button>
+        { goLogin && <Redirect to="/" /> }
+      </div>
     );
   }
 }
